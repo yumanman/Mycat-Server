a9455311b3d002b098ad0a30a4fa0a2d76578ab3
Mandyyu <Mandyyu@lagou.com>
2018-06-27 16:57:11 +0800

    本地化

---
+++ src/main/java/debug/MyCatStartUpTest.java
@@ -0,0 +1,19 @@
+package debug;
+
+import io.mycat.MycatStartup;
+
+/**
+ * All rights Reserved, Designed By www.lagou.com
+ * Title:  MyCatStartUpTest
+ * Package debug
+ * Description:
+ * Date:   2018/6/27 16:44
+ *
+ * @author mandy
+ * @version V1.0
+ */
+public class MyCatStartUpTest {
+    public static void main(String[] args) {
+        MycatStartup.main(args);
+    }
+}
